d559400384161a89293e00173f5e7ffdd843d4c6
Steve Moody <steve.moody2@gmail.com>
2018-12-30 19:04:26 -0700

    refactored PokeList fetchMore handler

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -7,62 +7,80 @@ import { GET_POKEMON } from '../utils/queries';
 import BusySpinner from './BusySpinner';
 
 class PokeList extends Component {
-  render() {
-    return (
-      <Query query={GET_POKEMON} variables={{ first: 50 }} >
-        {({loading, error, data, refetch, fetchMore}) => {
-            if (loading) {
-                console.log('loading: ', loading);
-                return <BusySpinner />
+    static propTypes = {
+
+    }
+    static defaultProps = {
+
+    }
+    render() {
+        return (
+            <Query query={GET_POKEMON} variables={{ first: 50, after: '' }} >
+                {({loading, error, data, refetch, fetchMore}) => {
+                    if (loading) {
+                        console.log('loading: ', loading);
+                        return <BusySpinner />
+                    }
+                    if (error) {
+                        console.log('error: ', error);
+                        return (
+                            <Fragment>
+                                <BusySpinner />
+                                <p>{error.message}</p>
+                            </Fragment>
+                        );
+                    }
+                    return (
+                        <div className="Poke-List">
+                            <button onClick={() => refetch()}>Refetch!</button>
+                            {_get(data, 'Pokemon.edges', []).map(edge =>
+                                <Pokemon
+                                    key={edge.cursor}
+                                    pokemon={edge.node}
+                                />
+                            )}
+                            <button
+                                onClick={() => this.handleLoadMore({data, fetchMore})}
+                            >Load More!</button>
+                        </div>
+                    );
+                }}
+            </Query>
+        );
+    }
+    /**
+     * handler for load more button, invokes Apollo#Query component fetchMore render prop
+     * @param {Object} param0
+     * @param {Object} param0.data
+     * @param {Object} param0.data.Pokemon
+     * @param {Object} param0.data.Pokemon.pageInfo
+     * @param {String} param0.data.Pokemon.pageInfo.endCursor
+     * @param {Function} param0.fetchMore
+     */
+    handleLoadMore({data, fetchMore}) {
+        return fetchMore({
+            variables: {
+                after: _get(data, 'Pokemon.pageInfo.endCursor', '')
+            },
+            updateQuery: (previousResult, { fetchMoreResult }) => {
+                const newEdges = fetchMoreResult.Pokemon.edges;
+                const pageInfo = fetchMoreResult.Pokemon.pageInfo;
+                const totalCount = fetchMoreResult.Pokemon.totalCount;
+                return newEdges.length
+                    ? {
+                        // Put the new pokemon at the end of the list and update `pageInfo`
+                        // so we have the new `endCursor` and `hasNextPage` values
+                        Pokemon: {
+                          __typename: previousResult.Pokemon.__typename,
+                          edges: [...previousResult.Pokemon.edges, ...newEdges],
+                          pageInfo,
+                          totalCount
+                        }
+                      }
+                    : previousResult;
             }
-            if (error) {
-                console.log('error: ', error);
-                return (
-                    <Fragment>
-                        <BusySpinner />
-                        <p>{error.message}</p>
-                    </Fragment>
-                );
-            }
-            return (
-                <div className="Poke-List">
-                    <button onClick={() => refetch()}>Refetch!</button>
-                    {_get(data, 'Pokemon.edges', []).map(edge =>
-                        <Pokemon
-                            key={edge.cursor}
-                            pokemon={edge.node}
-                        />
-                    )}
-                    <button
-                        onClick={() => fetchMore({
-                            variables: {
-                                after: data.Pokemon.pageInfo.endCursor
-                            },
-                            updateQuery: (previousResult, { fetchMoreResult }) => {
-                                const newEdges = fetchMoreResult.Pokemon.edges;
-                                const pageInfo = fetchMoreResult.Pokemon.pageInfo;
-                                const totalCount = fetchMoreResult.Pokemon.totalCount;
-                                return newEdges.length
-                                    ? {
-                                        // Put the new pokemon at the end of the list and update `pageInfo`
-                                        // so we have the new `endCursor` and `hasNextPage` values
-                                        Pokemon: {
-                                          __typename: previousResult.Pokemon.__typename,
-                                          edges: [...previousResult.Pokemon.edges, ...newEdges],
-                                          pageInfo,
-                                          totalCount
-                                        }
-                                      }
-                                    : previousResult;
-                            }
-                        })}
-                    >Load More!</button>
-                </div>
-            );
-        }}
-      </Query>
-    );
-  }
+        });
+    }
 }
 
 const Pokemon = ({pokemon}) => 
